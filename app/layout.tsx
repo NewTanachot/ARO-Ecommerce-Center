26f3e3dd024bdb39e04dacfcdb82ca9fc45eaab6
Tanachot <TanachotNew2543@gmail.com>
2023-05-05 00:33:08 +0700

    Icon Change - (4/5/23)

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import NavBar from '../component/navbar/navbar'
 import SideBar from '../component/sidebar/sidebar'
+import Icon from '../public/image/favicon.ico'
 import { Athiti } from 'next/font/google' // Sriracha, Itim, Kanit, Sarabun, Mali, Noto_Sans_Thai, Noto_Sans_Thai_Looped, 
 
 const googleFont = Athiti({
@@ -12,14 +13,13 @@ const googleFont = Athiti({
 
 export const metadata = {
   title: 'ARO Ecommerce Center',
-  description: 'ARO Ecommerce Center'
+  description: 'ARO Ecommerce Center',
+  icons: {
+    icon: Icon.src,
+  },
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode}) {
   return (
     <html lang="en">
         <body className={googleFont.className}>
@@ -31,9 +31,9 @@ export default function RootLayout({
                     </div>
                 </div>
                 <div className='col-9 p-0 bg-light border-start border-2 border-secondary'>
-                  <div className='p-2 pe-4'>
-                    {children}
-                  </div>
+                    <div className='p-2 pe-4'>
+                        {children}
+                    </div>
                 </div>
             </div>
         </body>
